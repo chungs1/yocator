f06ae1fba61eed5a3381b4f17730779f0238a5c2
SebastianMerz <Sebastian@sebastianmerz.com>
2014-06-27 19:59:30 -0700

    Working?

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 YO_UNSUBSCRIBED = 0
 YO_SUBSCRIBED = 1
 
@@ -7,14 +8,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     yo_name = db.Column(db.String(60), index=True, unique = True)
     subscribed = db.Column(db.SmallInteger, default=YO_SUBSCRIBED)
-    timestamp = Column(DateTime, nullable=False, server_default=func.now())
+    timestamp = db.Column(db.DateTime)
     yo_count = db.Column(db.Integer)
     db.relationship('Alert', backref='user', lazy = 'dynamic')
 
 class Yo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    timestamp = Column(DateTime, nullable=False, server_default=func.now())
-    user = db.Column(db.Integer, db.ForeignKey('user.id')
+    timestamp = db.Column(db.DateTime)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Stats(db.Model):
     id = db.Column(db.Integer, primary_key = True)
